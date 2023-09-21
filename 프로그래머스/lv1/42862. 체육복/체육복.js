e07fcc8f42b4da7446797ad8e787a2cf457bec6f
지혜 <77341912+wisdom-13@users.noreply.github.com>
2023-09-22 01:17:24 +0900

    [level 1] Title: 체육복, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -0,0 +1,27 @@
+function solution(n, lost, reserve) {
+    var answer = n;
+    var tmp = 0;
+    
+    var hap = lost.filter((i) => reserve.includes(i))
+    
+    lost = lost.filter((i) => !hap.includes(i)).sort((a, b) => a - b)
+    reserve = reserve.filter((i) => !hap.includes(i)).sort((a, b) => a - b)
+    
+    
+    for (i in lost) {
+         if (reserve.includes(lost[i] - 1)) {
+            removeItem(reserve, lost[i] - 1)
+        } else if (reserve.includes(lost[i] + 1)) {
+            removeItem(reserve, lost[i] + 1)
+        } else {
+            answer -= 1
+        }
+    }
+    
+    return answer;
+}
+
+
+function removeItem(arr, item) {
+    return arr.splice(arr.indexOf(item), 1)
+}
