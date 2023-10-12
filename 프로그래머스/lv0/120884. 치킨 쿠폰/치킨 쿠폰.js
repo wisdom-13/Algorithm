597f2448c1de3aa67d4dbb6a35997480cc6b6b68
지혜 <77341912+wisdom-13@users.noreply.github.com>
2023-10-12 14:26:13 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -0,0 +1,10 @@
+function solution(chicken) {
+    var answer = 0;
+    
+    while(chicken >= 10){
+    	answer += Math.floor(chicken / 10);
+        chicken = chicken % 10 + Math.floor(chicken / 10);
+    }
+
+    return answer
+}
