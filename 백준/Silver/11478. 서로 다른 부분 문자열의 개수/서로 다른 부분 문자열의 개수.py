544529ce0e41b0e40004d897943cae02afb2ac8b
지혜 <wisdom_13_@naver.com>
2024-05-30 12:24:53 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 2412 ms, Memory: 452860 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
@@ -1,7 +1,11 @@
 string = input()
 s = []
 
+# 1. 문자열의 길이만큼 반복
 for i in range(1, len(string) + 1):
-    s += [string[j:j+i] for j in range(0, len(string))]
 
+    # 2. 문자열 길이만큼 반복하면서, 한글자씩 옮겨가면서 i 길이만큼으로 문자열을 나눔
+    s += [string[j:j+i] for j in range(len(string))]
+
+# 3. set으로 중복을 제거한 후 길이 출력
 print(len(set(s)))
