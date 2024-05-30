fb27300f3cbd1d2f1b34d2abb32ab6ffcb41bf62
지혜 <wisdom_13_@naver.com>
2024-05-30 12:05:47 +0900

    [Silver III] Title: 파일 정리, Time: 236 ms, Memory: 44204 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.py
+++ 백준/Silver/20291. 파일 정리/파일 정리.py
@@ -5,10 +5,11 @@ input = sys.stdin.readline
 n = int(input())
 d = []
 
+# 1. 따옴표(.)를 기준으로 문자열을 나눈 후 확장자를 리스트에 저장
 for i in range(n):
     _, ext = input().rstrip().split('.')
     d.append(ext)
 
-# Counter를 정렬
+# 2. Counter로 리스트의 각 요소가 몇번씩 사용되었는지 받아온 후 정렬
 for k, v in sorted(Counter(d).items()):
     print(f"{k} {v}")
