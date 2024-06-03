c1ad35fe117a95e7b386df545fc522bf02329ed9
지혜 <wisdom_13_@naver.com>
2024-06-03 10:08:53 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -3,22 +3,16 @@ input = sys.stdin.readline
 
 N, M = map(int, input().rstrip().split())
 
-# 1. M * N의 이차원 리스트 생성
-list = [[0] * M for _ in range(N)]
+# 1. A 행렬의 값을 리스트에 저장
+list = [list(map(int, input().rstrip().split())) for _ in range(N)]
 
-# 2. A 행렬의 값을 리스트에 저장
-for i in range(N):
-    A_list = input().rstrip().split()
-    for j in range(M):
-        list[i][j] = int(A_list[j])
-
-# 3. B 행렬의 값을 기존 리스트의 값에 더하여 자장
+# 2. B 행렬의 값을 기존 리스트의 값에 더하여 자장
 for i in range(N):
     B_list = input().rstrip().split()
     for j in range(M):
         list[i][j] = list[i][j] + int(B_list[j])
 
-# 4. 출력
+# 3. 출력
 for i in list:
     for j in i:
         print(j, end=" ")
