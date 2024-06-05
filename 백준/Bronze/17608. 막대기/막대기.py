8364d308cd877b066afc009da82484e13e9bf14d
지혜 <wisdom_13_@naver.com>
2024-06-05 11:12:46 +0900

    [Bronze II] Title: 막대기, Time: 84 ms, Memory: 35756 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.py
+++ 백준/Bronze/17608. 막대기/막대기.py
@@ -1,4 +1,3 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
@@ -7,6 +6,7 @@ num = int(input().rstrip())
 list = [int(input().rstrip()) for _ in range(num)]
 max, cnt = 0, 0
 
+# 1. 배열을 뒤집어서 max 값과 비교, 더 크다면 max를 변경하고 cnt를 1 증가
 for i in list[::-1]:
     if i > max:
         cnt += 1
