38cb90da32d6980098e60bde21ecdff630c2a223
지혜 <wisdom_13_@naver.com>
2024-06-05 10:22:18 +0900

    [Bronze II] Title: 단어순서 뒤집기, Time: 52 ms, Memory: 33992 KB -BaekjoonHub

---
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.py
@@ -0,0 +1,11 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+num = int(input().rstrip())
+list = [input().rstrip().split() for _ in range(num)]
+
+# 배열의 순서를 뒤집어서 출력
+for entry in enumerate(list):
+    i, arr = entry
+    print(f'Case #{i+1}: {" ".join(arr[::-1])}')
