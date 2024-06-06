497d460c4863f2b741edf012ab0050f3b1e877b1
지혜 <wisdom_13_@naver.com>
2024-06-06 10:16:06 +0900

    [Bronze I] Title: 민균이의 비밀번호, Time: 60 ms, Memory: 34008 KB -BaekjoonHub

---
+++ 백준/Bronze/9933. 민균이의 비밀번호/민균이의 비밀번호.py
@@ -0,0 +1,23 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+num = int(input().rstrip())
+password_list = [input().rstrip() for _ in range(num)]
+
+
+# password_list에 문자열을 뒤집은 값이 있는지 체크하여 return
+def search_word(text):
+    text = text[::-1]
+    return text in password_list
+
+
+# 문자열의 길이와 중앙값을 return
+def text_len_median(text):
+    return len(text), text[len(text) // 2]
+
+
+for password in password_list:
+    if search_word(password):
+        print(*text_len_median(password))
+        break
