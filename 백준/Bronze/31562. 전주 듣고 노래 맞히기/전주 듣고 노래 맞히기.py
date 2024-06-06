f939408d975d819946531312d1bf43b004a3b543
지혜 <wisdom_13_@naver.com>
2024-06-06 11:42:17 +0900

    [Bronze I] Title: 전주 듣고 노래 맞히기, Time: 88 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/31562. 전주 듣고 노래 맞히기/전주 듣고 노래 맞히기.py
+++ 백준/Bronze/31562. 전주 듣고 노래 맞히기/전주 듣고 노래 맞히기.py
@@ -2,6 +2,8 @@ import sys
 input = sys.stdin.readline
 
 
+# 1. songs 딕셔너리에서 음이름의 첫 3개와 search_pitch가 일치하는 노래를 체크
+# 2. 없다면 !, 1개 이상이라면 ?, 1개라면 곡이름을 return
 def search_song(songs, search_pitch):
     result = '!'
     for title, pitch in songs.items():
