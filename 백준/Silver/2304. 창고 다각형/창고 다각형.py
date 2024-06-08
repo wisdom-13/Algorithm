ed5156124d95715f283c4c8eb6031e7048b22ead
지혜 <wisdom_13_@naver.com>
2024-06-08 23:52:43 +0900

    [Silver II] Title: 창고 다각형, Time: 44 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.py
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -5,6 +5,13 @@ input = sys.stdin.readline
 
 # 1. 중앙 기둥을 기준으로 오른쪽과 왼쪽 영역으로 나눔
 # 2. 각 영역을 각각의 끝에서부터 시작하면서 이전 기둥보다 높으면 높은 기둥 값 + 낮으면 이전 기둥값 +
+def get_area(start, end, step):
+    cnt, max_now = 0, 0
+    for s in range(start, end, step):
+        max_now = max(max_now, pillar.get(s, max_now))
+        cnt += max_now
+    return cnt
+
 
 N = int(input().rstrip())
 pillar = {}
@@ -22,17 +29,8 @@ for i in range(N):
     end = max(end, L)
     pillar[L] = H
 
-# 시작 위치부터 가장 높은 기둥까지
-max_now = 0
-for s in range(start, max_l):
-    max_now = max(max_now, pillar.get(s, max_now))
-    cnt += max_now
+total_area = max_h
+total_area += get_area(start, max_l, 1)
+total_area += get_area(end, max_l, -1)
 
-
-# 끝 위치부터 가장 높은 기둥까지
-max_now = 0
-for s in range(end, max_l, -1):
-    max_now = max(max_now, pillar.get(s, max_now))
-    cnt += max_now
-
-print(cnt+max_h)
+print(total_area)
